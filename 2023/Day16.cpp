2603f4864f6265bb4ad6b0133f6103fd23118766
LankyLuke97 <lukehowden@rocketmail.com>
2024-01-07 16:14:20 +0000

    Day 16, part 2
    Not the fastest solution, but runs in less than 10 seconds. Would need
    to look (likely) at recursive memoisation solution, much like the Hot
    Springs problem, to solve this quickly.

--- 2023/Day16.cpp
+++ 2023/Day16.cpp
@@ -142,6 +142,231 @@ int Day16::calculatePuzzle1(std::vector<std::string> input) {
 int Day16::calculatePuzzle2(std::vector<std::string> input) {
 	int answer = 0;
 
+	std::string flattened = "";
+	for (std::string line : input) {
+		if (line.empty())break;
+		flattened += line;
+	}
+
+	int xSize = input[0].size(), ySize = input.size() - 1;
+	std::vector<Beam> startingBeams;
+
+	for (int i = 0; i < xSize; i++) {
+		switch (flattened[i]) {
+		case '.':
+		case '|':
+			startingBeams.push_back(Beam(i, 2));
+			break;
+		case '-':
+			startingBeams.push_back(Beam(i, 1));
+			startingBeams.push_back(Beam(i, 3));
+			break;
+		case '/':
+			startingBeams.push_back(Beam(i, 3));
+			break;
+		case '\\':
+			startingBeams.push_back(Beam(i, 1));
+			break;
+		}
+
+		int pos = ((ySize - 1) * xSize) + i;
+
+		switch (flattened[pos]) {
+		case '.':
+		case '|':
+			startingBeams.push_back(Beam(pos, 2));
+			break;
+		case '-':
+			startingBeams.push_back(Beam(pos, 1));
+			startingBeams.push_back(Beam(pos, 3));
+			break;
+		case '/':
+			startingBeams.push_back(Beam(pos, 3));
+			break;
+		case '\\':
+			startingBeams.push_back(Beam(pos, 1));
+			break;
+		}
+	}
+
+	for (int i = 0; i < ySize; i++) {
+		int pos = (xSize * i);
+		switch (flattened[pos]) {
+		case '.':
+		case '-':
+			startingBeams.push_back(Beam(pos, 1));
+			break;
+		case '|':
+			startingBeams.push_back(Beam(pos, 0));
+			startingBeams.push_back(Beam(pos, 2));
+			break;
+		case '/':
+			startingBeams.push_back(Beam(pos, 0));
+			break;
+		case '\\':
+			startingBeams.push_back(Beam(pos, 2));
+			break;
+		}
+
+		pos = (xSize * (i + 1)) - 1;
+
+		switch (flattened[pos]) {
+		case '.':
+		case '-':
+			startingBeams.push_back(Beam(pos, 3));
+			break;
+		case '|':
+			startingBeams.push_back(Beam(pos, 0));
+			startingBeams.push_back(Beam(pos, 2));
+			break;
+		case '/':
+			startingBeams.push_back(Beam(pos, 2));
+			break;
+		case '\\':
+			startingBeams.push_back(Beam(pos, 0));
+			break;
+		}
+	}
+
+	for (Beam starting : startingBeams) {
+		std::vector<int> energised{ starting.position };
+		std::vector<Beam> beams{ starting };
+		std::vector<Beam> reviewed;
+
+		int beamIndex = 0;
+
+		while (beamIndex < beams.size()) {
+			if (std::find(reviewed.begin(), reviewed.end(), beams[beamIndex]) != reviewed.end()) {
+				beamIndex++;
+				continue;
+			}
+			reviewed.push_back(beams[beamIndex]);
+			int currentPos = beams[beamIndex].position, currentDirection = beams[beamIndex].direction;
+			int newPos = -1;
+			char c;
+			switch (currentDirection) {
+			case 0:
+				if (currentPos < xSize) {
+					beamIndex++;
+					break;
+				}
+				newPos = currentPos - xSize;
+				if (std::find(energised.begin(), energised.end(), newPos) == energised.end()) energised.push_back(newPos);
+				c = flattened[newPos];
+				if (c == '.' or c == '|') {
+					beams[beamIndex].position = newPos;
+				}
+				else if (c == '-') {
+					Beam left(newPos, 3), right(newPos, 1);
+					if (std::find(beams.begin(), beams.end(), left) == beams.end()) beams.push_back(left);
+					if (std::find(beams.begin(), beams.end(), right) == beams.end()) beams.push_back(right);
+					beamIndex++;
+				}
+				else if (c == '/') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 1;
+				}
+				else if (c == '\\') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 3;
+				}
+				break;
+			case 1:
+				if (currentPos % xSize == (xSize - 1)) {
+					beamIndex++;
+					break;
+				}
+				newPos = currentPos + 1;
+				if (std::find(energised.begin(), energised.end(), newPos) == energised.end()) energised.push_back(newPos);
+				c = flattened[newPos];
+				if (c == '.' or c == '-') {
+					beams[beamIndex].position = newPos;
+					break;
+				}
+				else if (c == '|') {
+					Beam up(newPos, 0), down(newPos, 2);
+					if (std::find(beams.begin(), beams.end(), up) == beams.end()) beams.push_back(up);
+					if (std::find(beams.begin(), beams.end(), down) == beams.end()) beams.push_back(down);
+					beamIndex++;
+					break;
+				}
+				else if (c == '/') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 0;
+					break;
+				}
+				else if (c == '\\') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 2;
+					break;
+				}
+				break;
+			case 2:
+				if (currentPos >= (ySize - 1) * xSize) {
+					beamIndex++;
+					break;
+				} 
+				newPos = currentPos + xSize;
+				if (std::find(energised.begin(), energised.end(), newPos) == energised.end()) energised.push_back(newPos);
+				c = flattened[newPos];
+				if (c == '.' or c == '|') {
+					beams[beamIndex].position = newPos;
+				}
+				else if (c == '-') {
+					Beam left(newPos, 3), right(newPos, 1);
+					if (std::find(beams.begin(), beams.end(), left) == beams.end()) beams.push_back(left);
+					if (std::find(beams.begin(), beams.end(), right) == beams.end()) beams.push_back(right);
+					beamIndex++;
+				}
+				else if (c == '/') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 3;
+				}
+				else if (c == '\\') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 1;
+				}
+				break;
+			case 3:
+				if (currentPos % xSize == 0) {
+					beamIndex++;
+					break;
+				}
+				newPos = currentPos - 1;
+				if (std::find(energised.begin(), energised.end(), newPos) == energised.end()) energised.push_back(newPos);
+				c = flattened[newPos];
+				if (c == '.' or c == '-') {
+					beams[beamIndex].position = newPos;
+					break;
+
+				}
+				else if (c == '|') {
+					Beam up(newPos, 0), down(newPos, 2);
+					if (std::find(beams.begin(), beams.end(), up) == beams.end()) beams.push_back(up);
+					if (std::find(beams.begin(), beams.end(), down) == beams.end()) beams.push_back(down);
+					beamIndex++;
+					break;
+
+				}
+				else if (c == '/') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 2;
+					break;
+
+				}
+				else if (c == '\\') {
+					beams[beamIndex].position = newPos;
+					beams[beamIndex].direction = 0;
+					break;
+				}
+				break;
+			default:
+				break;
+			}
+		}
+		answer = max(answer, energised.size());
+	}
+
 	return answer;
 }
 
@@ -163,7 +388,7 @@ void Day16::test() {
 	std::cout << "Day 16 part 1 test passed" << std::endl;
 	SetConsoleTextAttribute(h, 7);
 	SetConsoleTextAttribute(h, 4);
-	assert(calculatePuzzle2(Reader::readFile(testFile2)) == 0);
+	assert(calculatePuzzle2(Reader::readFile(testFile2)) == 51);
 	SetConsoleTextAttribute(h, 2);
 	std::cout << "Day 16 part 2 test passed" << std::endl;
 	SetConsoleTextAttribute(h, 7);
