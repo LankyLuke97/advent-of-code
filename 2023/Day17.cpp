342cddaa5b86fd22f3f1e077d9e5530bc37e4ceb
LankyLuke97 <lukehowden@rocketmail.com>
2024-01-10 20:45:20 +0000

    Day 17, part 1 incomplete
    Switching to a graph based approach to implement Djikstra's algorithm.

--- 2023/Day17.cpp
+++ 2023/Day17.cpp
@@ -4,24 +4,100 @@
 #include <Windows.h>
 #include "Day17.h"
 
-std::unordered_map<std::string, int> Day17::costs;
-std::vector<Step> Day17::visitedSteps;
-
 int Day17::calculatePuzzle1(std::vector<std::string> input) {
 	int answer = 0;
-	Day17::costs.clear();
-
 	int horizontalSize = input[0].size(), verticalSize = input.size() - 1;
-	
+	std::vector<Node> graph(12 * (verticalSize * horizontalSize));
+
+	for (int i = 0; i < verticalSize;i++) {
+		if (input[i].empty()) break;
+		for (int j = 0; j < horizontalSize; j++)
+			for(int k = 0; k < 12; k++)
+				graph.push_back(Node((i * horizontalSize) + (j * 12) + k, k));
+	}
+
+	/*
+	0 - up one
+	1 - right one
+	2 - down one
+	3 - left one
+	4..7 - two
+	8..11 - three
+	*/
+
+	for (int i = 0; i < verticalSize; i++) {
+		if (input[i].empty()) break;
+
+		for (int j = 0; j < horizontalSize; j++) {
+			for (int k = 0; k < 12; k++) {
+				int pos = (i * horizontalSize) + (j * 12) + k;
+				if (i > 0 && graph[pos].prevStep != 8) {
+					graph[pos - (12 * verticalSize)].weight = input[i - 1][j];
+					graph[pos].connectedNodes.push_back(graph[pos - (12 * verticalSize)]);
+
+					if (i > 1 && graph[pos].prevStep != 4) {
+						graph[pos - (24 * verticalSize)].weight = input[i - 2][j];
+						graph[pos].connectedNodes.push_back(graph[pos - (24 * verticalSize)]);
+
+						if (i > 2 && graph[pos].prevStep != 0) {
+							graph[pos - (36 * verticalSize)].weight = input[i - 3][j];
+							graph[pos].connectedNodes.push_back(graph[pos - (36 * verticalSize)]);
+						}
+					}
+				}
+
+				if (j > 0) {
+
+				}
+				if (j > 1) {
+
+				}
+				if (j > 2) {
+
+				}
+
+				if (i < verticalSize - 1) {
+
+				}
+				if (i < verticalSize - 2) {
+
+				}
+				if (i < verticalSize - 3) {
+
+				}
+
+				if (j < verticalSize - 1) {
+
+				}
+				if (j < verticalSize - 2) {
+
+				}
+				if (j < verticalSize - 3) {
+
+				}
+			}
+		}
+	}
+
 	std::string flattened = "";
 	for (std::string line : input) {
-		if (line.empty())break;
+		if (line.empty()) break;
 		flattened += line;
 	}
 
-	answer = min(recursePuzzle1(flattened, std::vector<Step>{Step(0, 1)}, horizontalSize, verticalSize), recursePuzzle1(flattened, std::vector<Step>{Step(0, 2)}, horizontalSize, verticalSize));
+	std::vector<int> costs(flattened.size(), INT_MAX);
+	costs[0] = 0;
+	std::vector<int> visitedNodes{0};
+	std::vector<int> unvisitedNodes;
+	int currentNode = 0;
 
-	std::cout << "ANSWER: " << answer << std::endl;
+	for (int i = 1; i < flattened.size(); i++) {
+		unvisitedNodes.push_back(i);
+	}
+
+	while (!unvisitedNodes.empty()) {
+
+	}
 
 	return answer;
 }
@@ -29,109 +105,7 @@ int Day17::calculatePuzzle1(std::vector<std::string> input) {
 int Day17::calculatePuzzle2(std::vector<std::string> input) {
 	int answer = 0;
 
-
 	return answer;
-}
-
-int Day17::recursePuzzle1(const std::string& input, std::vector<Step> steps, int horizontalSize, int verticalSize) {
-	int cost = INT_MAX;
-
-	if (steps.size() == 0) return cost;
-
-	/*std::cout << "CURRENT STEPS: " << std::endl;
-	for (Step step : steps) {
-		std::cout << step.toString() << "\n";
-	}
-	std::cout << std::endl;*/
-
-	Step lastStep = steps[steps.size() - 1];
-	auto search = std::find_if(
-		Day17::costs.begin(), Day17::costs.end(),
-		[lastStep](const auto& element) {
-			return element.first == lastStep.toString();
-		}
-	);
-
-	if (lastStep.position == 27 && lastStep.direction == 3)
-		std::cout << "Here we go" << std::endl;
-
-	if (search != Day17::costs.end()) {
-		return search->second;
-	}
-
-	if (lastStep.position == input.size() - 1) {
-		std::cout << "End: " << *(input.end() - 1) << ", " << lastStep.toString() << std::endl;
-		Day17::costs.emplace(lastStep.toString(), *(input.end() - 1));
-		return *(input.end() - 1);
-	}
-
-	for (int dir = 0; dir < 4; dir++) {
-		bool crash = true;
-		for (int i = 2; i < 5; i++) {
-			int index = steps.size() - i;
-			if (index < 0) {
-				crash = false;
-				break;
-			}
-			if (steps[index].direction != lastStep.direction) crash = false;
-		}
-		if (crash) continue;
-
-		std::vector<Step> newSteps;
-		bool added = false;
-		Step newStep(-1, -1);
-
-		switch (dir) {
-		case 0: {
-			if (lastStep.position < horizontalSize || lastStep.direction == 2) break;
-			newSteps = steps;
-			newStep = Step(lastStep.position - horizontalSize, dir);
-			if (std::find(Day17::visitedSteps.begin(), Day17::visitedSteps.end(), newStep) != Day17::visitedSteps.end()) break;
-			added = true;
-			newSteps.push_back(newStep);
-			Day17::visitedSteps.push_back(newStep);
-			break;
-		}
-		case 1: {
-			if ((lastStep.position % horizontalSize) >= horizontalSize - 1 || lastStep.direction == 3) break;
-			newSteps = steps;
-			newStep = Step(lastStep.position + 1, dir);
-			if (std::find(Day17::visitedSteps.begin(), Day17::visitedSteps.end(), newStep) != Day17::visitedSteps.end()) break;
-			added = true;
-			newSteps.push_back(newStep);
-			Day17::visitedSteps.push_back(newStep);
-			break;
-		}
-		case 2: {
-			if (lastStep.position >= (verticalSize - 1) * horizontalSize || lastStep.direction == 0) break;
-			newSteps = steps;
-			newStep = Step(lastStep.position + horizontalSize, dir);
-			if (std::find(Day17::visitedSteps.begin(), Day17::visitedSteps.end(), newStep) != Day17::visitedSteps.end()) break;
-			added = true;
-			newSteps.push_back(newStep);
-			Day17::visitedSteps.push_back(newStep);
-			break;
-		}
-		case 3: {
-			if ((lastStep.position % horizontalSize) <= 0 || lastStep.direction == 1) break;
-			newSteps = steps;
-			newStep = Step(lastStep.position - 1, dir);
-			if (std::find(Day17::visitedSteps.begin(), Day17::visitedSteps.end(), newStep) != Day17::visitedSteps.end()) break;
-			added = true;
-			newSteps.push_back(newStep);
-			Day17::visitedSteps.push_back(newStep);
-			break;
-		}
-		}
-
-		if (cost == -2147483498) 
-			std::cout << "NAH BRAH: " << lastStep.toString() << std::endl;
-		else if (lastStep.position == 27 && lastStep.direction == 3) std::cout << "Hmmmmmmmmm" << std::endl;
-		if(added) cost = min(cost, input[lastStep.position] + recursePuzzle1(input, newSteps, horizontalSize, verticalSize));
-	}
-
-	Day17::costs.emplace(lastStep.toString(), cost);
-	return cost;
 }
 
 void Day17::puzzle1() {
--- 2023/Day17.h
+++ 2023/Day17.h
@@ -1,32 +1,22 @@
 #pragma once
-#include <unordered_map>
 #include "Reader.h"
 
-struct Step {
-	int direction;
+struct Node {
 	int position;
+	int weight;
+	int prevStep;
+	std::vector<Node> connectedNodes;
 
-	Step(int _pos, int _dir) : position(_pos), direction(_dir) {}
-
-	friend bool operator == (Step const& l, Step const& r) {
-		return l.direction == r.direction && l.position == r.position;
-	}
-
-	std::string toString() const {
-		return std::to_string(position) + "," + std::to_string(direction);
-	}
+	Node(int _pos, int _prevStep) : position(_pos), prevStep(_prevStep), connectedNodes(std::vector<Node>(12)) {}
 };
 
 class Day17 {
 private:
-	static std::unordered_map<std::string, int> costs;
-	static std::vector<Step> visitedSteps;
 	const std::string testFile1 = "inputs/day17_part1_test.txt";
 	const std::string testFile2 = "inputs/day17_part2_test.txt";
 	const std::string puzzleFile = "inputs/day17.txt";
 	int calculatePuzzle1(std::vector<std::string> input);
 	int calculatePuzzle2(std::vector<std::string> input);
-	int recursePuzzle1(const std::string& input, std::vector<Step> steps, int horizontalSize, int verticalSize);
 public:
 	void puzzle1();
 	void puzzle2();
