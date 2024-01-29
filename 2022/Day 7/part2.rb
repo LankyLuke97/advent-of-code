ddf546bcb726438082501888ec52aa68e247f8e8
LankyLuke97 <lukehowden@rocketmail.com>
2024-01-29 17:45:07 +0000

    Day 7, part 2

---
+++ 2022/Day 7/part2.rb
@@ -0,0 +1,73 @@
+require 'Set'
+
+$dirStack = ['/']
+$dirs = Set.new
+$dirDepth = Hash.new()
+$dirMappings = Hash.new([])
+$dirSizes = Hash.new(0)
+$finalisedDirSizes = Hash.new
+
+sum = 0
+MAX_SIZE = 100000
+
+def createPath(stack)
+    return stack.join("\\")
+end
+
+def findDirSize(dir)
+    if $finalisedDirSizes.has_key?(dir)
+        return $finalisedDirSizes[dir]
+    end
+
+    size = $dirSizes[dir]
+    unless $dirMappings[dir].nil? 
+        $dirMappings[dir].each do |subDir|
+            size += findDirSize(subDir)
+        end
+    end
+    $finalisedDirSizes[dir] = size
+    return size
+end
+
+filename = ARGV.length() == 0 ? "input.txt" : ARGV[0].to_s
+$dirs.add("/")
+
+File.readlines(filename).each do |line|
+    tokens = line.strip.split
+
+    if tokens[0] == '$'
+        if tokens[1] == 'cd'
+            if tokens[2] == '..'
+                $dirStack.pop
+            elsif tokens[2] == '/'
+                $dirStack = ['/']
+            else
+                $dirStack << tokens[2]
+            end
+        end
+    else
+        path = createPath($dirStack)
+        if tokens[0] == 'dir'
+            newDir = path + "\\" + tokens[1]
+            $dirs.add(newDir)
+            $dirMappings[path] += [newDir]
+        else
+            $dirSizes[path] += tokens[0].to_i
+        end
+    end
+end
+
+sum = 0
+$dirs.each do |key, val|
+    $dirs.add(key)
+    findDirSize(key)
+end
+
+MAX_AVAILABLE = 70000000
+MIN_REQUIRED = 30000000
+
+currentlyAvailable = MAX_AVAILABLE - $finalisedDirSizes["/"]
+minDeletion = MIN_REQUIRED - currentlyAvailable
+
+deleteDirSize = $finalisedDirSizes.select { |key, value| value >= minDeletion }.min_by { |key, value| value }[1]
+puts"#{deleteDirSize}"
