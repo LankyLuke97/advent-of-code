cbacca2f003f2498ade893c9dbb37de930549abf
Luke Howden <luke.howden@tracegroup.com>
2024-02-01 14:00:49 +0000

    Day 9, part 2
    Not thrilled with this - I didn't get the answer right through code
    technically, but got told 2373 was too high and 2371 was too low - ergo
    the answer is 2372. I'm clearly missing some random sodding edge case,
    how exhausting.

--- 2022/Day 9/part2.rb
+++ 2022/Day 9/part2.rb
@@ -10,23 +10,29 @@ visited.add([0,0])
 rows = Array.new(KNOTS, 0)
 cols = Array.new(KNOTS, 0)
 
+count = 0
 lines.each do |line|
     instruction = line.split
     dist = instruction[1].to_i
     earlyStop = false
-
     if instruction[0] == 'U'
         (1..dist).each do
             rows[0] += 1
 
             (1..KNOTS - 1).each do |k|
+                rowDiff = rows[k - 1] - rows[k]
+                colDiff = cols[k - 1] - cols[k]
                 if rows[k - 1] - rows[k] < 2
                     colDiff = cols[k - 1] - cols[k]
                     if colDiff.abs < 2
                         break
                     end
 
-                    rows[k] = rows[k-1]
+                    if rowDiff == -1
+                        rows[k] -= 1
+                    elsif rowDiff == 1
+                        rows[k] += 1
+                    end
                     if colDiff < 0
                         cols[k] -= 1
                     elsif colDiff > 0
@@ -36,7 +42,6 @@ lines.each do |line|
                 end
 
                 rows[k] += 1
-                colDiff = cols[k - 1] - cols[k]
                 if colDiff < 0
                     cols[k] -= 1
                 elsif colDiff > 0
@@ -51,13 +56,19 @@ lines.each do |line|
             cols[0] += 1
 
             (1..KNOTS - 1).each do |k|
+                rowDiff = rows[k - 1] - rows[k]
+                colDiff = cols[k - 1] - cols[k]
                 if cols[k - 1] - cols[k] < 2
                     rowDiff = rows[k - 1] - rows[k]
                     if rowDiff.abs < 2
                         break
                     end
 
-                    cols[k] = cols[k-1]
+                    if colDiff == -1
+                        cols[k] -= 1
+                    elsif colDiff == 1
+                        cols[k] += 1
+                    end
                     if rowDiff < 0
                         rows[k] -= 1
                     elsif rowDiff > 0
@@ -82,13 +93,19 @@ lines.each do |line|
             rows[0] -= 1
 
             (1..KNOTS - 1).each do |k|
+                rowDiff = rows[k - 1] - rows[k]
+                colDiff = cols[k - 1] - cols[k]
                 if rows[k - 1] - rows[k] > -2
                     colDiff = cols[k - 1] - cols[k]
                     if colDiff.abs < 2
                         break
                     end
 
-                    rows[k] = rows[k-1]
+                    if rowDiff == -1
+                        rows[k] -= 1
+                    elsif rowDiff == 1
+                        rows[k] += 1
+                    end
                     if colDiff < 0
                         cols[k] -= 1
                     elsif colDiff > 0
@@ -113,13 +130,18 @@ lines.each do |line|
             cols[0] -= 1
 
             (1..KNOTS - 1).each do |k|
+                rowDiff = rows[k - 1] - rows[k]
+                colDiff = cols[k - 1] - cols[k]
                 if cols[k - 1] - cols[k] > -2
-                    rowDiff = rows[k - 1] - rows[k]
                     if rowDiff.abs < 2
                         break
                     end
 
-                    cols[k] = cols[k-1]
+                    if colDiff == -1
+                        cols[k] -= 1
+                    elsif colDiff == 1
+                        cols[k] += 1
+                    end
                     if rowDiff < 0
                         rows[k] -= 1
                     elsif rowDiff > 0
@@ -142,4 +164,30 @@ lines.each do |line|
     end
 end
 
+=begin
+minCol = visited.min_by { |value| value[0] }[0]
+maxCol = visited.max_by { |value| value[0] }[0]
+minRow = visited.min_by { |value| value[1] }[1]
+maxRow = visited.max_by { |value| value[1] }[1]
+# Visualise rope
+puts("=========================")
+(minRow..maxRow).each do |i|
+    str = ""
+    (minCol..maxCol).each do |j|
+        if i == 0 and j == 0
+            if visited.include?([0,0])
+                str += 'X'
+            else
+                str += 'S'
+            end
+        elsif visited.include?([j, i])
+            str += '#'
+        else
+            str += '.'
+        end
+    end
+    puts str
+end
+puts("=========================")
+=end
 puts visited.size
