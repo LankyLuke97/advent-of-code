c5577fd4e17061dceef02141976bd37db0d17151
LankyLuke97 <lukehowden@rocketmail.com>
2024-02-02 09:35:00 +0000

    Day 11, part 1

--- 2022/Day 11/part1.rb
+++ 2022/Day 11/part1.rb
@@ -21,7 +21,7 @@ lines.each do |line|
     elsif line[0].include?("Operation")
         op = line[1].split
         opEnum = -1
-        if op[4] != "old"
+        if op[4] == "old"
             opEnum = 2
         elsif op[3] == '+'
             opEnum = 0
@@ -39,7 +39,7 @@ lines.each do |line|
 end
 
 items.each_with_index do |item, index|
-    puts "Monkey #{index}: #{item.join(",")}"
+    # puts "Monkey #{index}: #{item.join(",")}"
 end
 
 inspected = Array.new(items.size, 0)
@@ -49,35 +49,40 @@ inspected = Array.new(items.size, 0)
     items.each_with_index do |item, monkeyIndex|
         operation = operations[monkeyIndex]
         t = tests[monkeyIndex]
+        # puts "Monkey #{monkeyIndex}:"
         while(!item.empty?)
             newItem = item.pop
+            # puts "\tMonkey inspects an item with a worry level of #{newItem}"
             inspected[monkeyIndex] += 1
-            #puts "Here: #{newItem}"
             if operation[0] == 0
                 newItem += operation[1]
+                # puts "\t\tWorry level increases by #{operation[1]} to #{newItem}"
             elsif operation[0] == 1
                 newItem *= operation[1]
+                # puts "\t\tWorry level is multiplied by #{operation[1]} to #{newItem}"
             else
                 newItem = newItem * newItem
+                # puts "\t\tWorry level is multiplied by itself to #{newItem}"
             end
-            #puts "There: #{newItem}"
             newItem = (newItem / 3).floor
-            #puts "Finally: #{newItem}"
+            # puts "\t\tMonkey gets bored with item. Worry level is divided by 3 to #{newItem}"
 
             if newItem % t[0] == 0
                 items[t[1]].unshift(newItem)
+                # puts "\t\tCurrent worry level is divisible by #{t[0]}"
+                # puts "\t\tItem with worry level #{newItem} is thrown to monkey #{t[1]}"
             else
                 items[t[2]].unshift(newItem)
+                # puts "\t\tCurrent worry level is not ivisible by #{t[0]}"
+                # puts "\t\tItem with worry level #{newItem} is thrown to monkey #{t[2]}"
             end
         end
     end
-    #puts "After round #{r}"
-    #items.each_with_index do |item, index|
-    #    puts "Monkey #{index}: #{item.join(",")}"
-    #end
+    # puts "After round #{r}"
+    items.each_with_index do |item, index|
+        # puts "Monkey #{index}: #{item.join(",")}"
+    end
 end
-
-puts inspected.join(", ")
 
 inspected.sort! {|x, y| y <=> x}
 monkeyBusiness = inspected[0] * inspected[1] #lol
