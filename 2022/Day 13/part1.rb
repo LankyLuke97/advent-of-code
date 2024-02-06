ce18dbecebcc990b50d82c07e4bca5b20eafa7ba
LankyLuke97 <lukehowden@rocketmail.com>
2024-02-06 10:34:04 +0000

    Day 13, part 1

--- 2022/Day 13/part1.rb
+++ 2022/Day 13/part1.rb
@@ -1,77 +1,62 @@
 require 'strscan'
 
-def parseString(input)
+def parseString(input, depth=0)
     result = []
-    current_list = result
-  
+
     scanner = StringScanner.new(input)
-  
+
     until scanner.eos?
         case
         when scanner.scan(/\[/)
-            current_list << parseString(scanner.scan_until(/\]/))
+            nested_list = parseString(scanner.scan_until(/\]/), depth + 1)
+            result << nested_list
+            
         when scanner.scan(/\d+/)
-            current_list << scanner.matched.to_i
+            result << scanner.matched.to_i
         when scanner.scan(/,/)
             # Ignore commas
         when scanner.scan(/\s+/)
             # Ignore whitespace
         when scanner.scan(/\]/)
-            # Ignore commas
+            break  # Exit the loop when encountering ]
         else
             raise "Unexpected character: #{scanner.peek(1)}"
         end
     end
-  
+
     result
 end
 
-def compare(list1, list2, canConvert)
-    puts "Compare #{list1.inspect} vs #{list2.inspect}"
-    if !!list1.is_a?(Array) ^ !!list2.is_a?(Array)
-        unless canConvert    
-            return false
+def compare(list1, list2)
+    if list1.is_a?(Integer) and list2.is_a?(Integer)
+        if list1 < list2
+            return 1
+        elsif list1 > list2
+            return -1
         end
+        return 0                    
+    end
 
+    if !!list1.is_a?(Array) ^ !!list2.is_a?(Array)
         if list1.is_a?(Array)
-            puts "Mixed types; convert right to [#{list2}] and retry comparison"
-            return compare(list1, [list2], false)
+            return compare(list1, [list2])
         else
-            puts "Mixed types; convert left to [#{list1}] and retry comparison"
-            return compare([list1], list2, false)
+            return compare([list1], list2)
         end
     end
 
     list1.each_with_index do |l1, index|
         if index >= list2.size
-            puts "Right side ran out of items, so inputs are not in the right order"
-            return false
+            break
         end
-        
-        l2 = list2[index]
 
-        if l1.is_a?(Integer) && l2.is_a?(Integer)
-            puts "Compare #{l1} vs #{l2}"
-
-            if l1 > l2
-                puts "Right side is smaller, so inputs are not in the right order"
-                return false
-            end
-
-            if(l1 < l2)
-                puts "Left side is smaller, so inputs are in the right order"
-                return true
-            end
-        else
-            if !compare(l1, l2, canConvert)
-                return false
-            end
+        res = compare(list1[index], list2[index])
+        unless res == 0
+            return res
         end
     end
-    
-    puts "Left side ran out of items, so inputs are in the right order"
-    
-    true
+
+    return compare(list1.size, list2.size)
 end
 
 filename = ARGV.length() == 0 ? "input.txt" : ARGV[0].to_s
@@ -81,32 +66,31 @@ lines = File.readlines(filename)
 left = nil
 right = nil
 index = 0
-correct = 0
+sum = 0
+correct = []
+wrong = []
 
 lines.each do |line|
     line.strip!
+    if line.nil? or line.empty?
+        next
+    end
+
     if left.nil?
         left = parseString(line)
         next
     end
 
-    if right.nil?
-        right = parseString(line)
-        next
-    end
+    right = parseString(line)
 
     index += 1
 
-    puts "== Pair #{index} =="
-
-    if compare(left, right, true)
-        correct += index
+    if compare(left, right) == 1
+        sum += index
     end
-
-    puts "\n"
 
     left = nil
     right = nil
 end
 
-puts correct
+puts sum
