096ee8178fe0a01b07db476d495c5a3807d85255
LankyLuke97 <lukehowden@rocketmail.com>
2024-02-27 09:59:25 +0000

    Day 19, part 1 incomplete
    More minor optimisations, still very slow

--- 2022/Day 19/part1.rb
+++ 2022/Day 19/part1.rb
@@ -12,9 +12,11 @@ blueprints.each do |blueprint|
     resources = [[0, 0, 0, 0, 1, 0, 0, 0]] # ore, clay, obsidian, geodes, repeat for robot types
     costs = [[blueprint[1], 0, 0, 0], [blueprint[2], 0, 0, 0], [blueprint[3], blueprint[4], 0, 0], [blueprint[5], 0, blueprint[6], 0]]
     maxCosts = costs.transpose.map(&:max)
+    maxCosts[3] = 10000
 
     (24..0).step(-1).each do |i|
         newResources = Array.new
+        currentMax = resources.max_by{|resource| resource[3]}[3]
         resources.each do |resource|
             have = resource[0..3]
             make = resource[4..7]
@@ -22,10 +24,13 @@ blueprints.each do |blueprint|
                 unless cost.zip(have).any? {|c, h| c > h }
                     newMake = make.dup
                     newMake[increase] += 1
-                    if increase != 3 and newMake[increase] > maxCosts[increase]
+                    if newMake[increase] > maxCosts[increase]
                         next
                     end
                     r = have.zip(make).map {|h, m| h + m}.zip(cost).map { |n, c| n - c} + newMake
+                    if (((i - 1) * i) / 2) + r[3] + (i * r[7]) < currentMax
+                        next
+                    end
                     newResources.append(r)
                 end
             end
@@ -33,6 +38,7 @@ blueprints.each do |blueprint|
             newResources.append(r)
         end
         resources = newResources.dup
+        puts "With #{i} minutes remaining, #{resources.size} resources to check"
     end
 
     max = resources.max_by{|resource| resource[3]}[3]
