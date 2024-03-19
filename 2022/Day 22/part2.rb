27a98c14c746703701d72a8c500b7fc07b75f37c
LankyLuke97 <lukehowden@rocketmail.com>
2024-03-19 15:40:59 +0000

    Day 22, part 2 incomplete
    Begin cube decoding

--- 2022/Day 22/part2.rb
+++ 2022/Day 22/part2.rb
@@ -1,6 +1,9 @@
+require 'Set'
+
 filename = ARGV.length() == 0 ? "input.txt" : ARGV[0].to_s
 lines = File.readlines(filename)
 
+cubeSize = filename == "input.txt" ? 50 : 4
 pos = [0,lines[0].index('.')]
 lengthPad = lines[0..-3].max_by(&:length).length - 1
 directions = Hash.new
@@ -12,6 +15,57 @@ tops = Array.new(lengthPad, -1)
 rights = Array.new(lines.size - 2, -1)
 bottoms = Array.new(lengthPad, -1)
 cleaned = []
+
+def wrapCube(cubeSize, lines)
+    innerCorners = Set.new
+
+    lines.each_with_index do |line, y|
+        if y == 0 or y == (lines.size - 1)
+            next
+        end
+
+        line.each_char.with_index do |c, x|
+            if x == 0 or x == (line.size - 1)
+                next
+            end
+
+            numEmpty = 0
+
+            (-1..1).each do |i|
+                (-1..1).each do |j|
+                    if lines[y+j][x+i] == ' '
+                        numEmpty+= 1
+                    end
+                end
+            end
+
+            if numEmpty == 1
+                if x % cubeSize == (cubeSize - 1)
+                    x+= 1
+                end
+                if y % cubeSize == (cubeSize - 1)
+                    y+= 1
+                end
+                
+                innerCorners << [x, y]
+            end
+        end
+    end
+    
+    innerCorners = innerCorners.to_a
+
+    # The below could be fleshed out for all the cube unfoldings
+    if innerCorners.size == 2
+        puts "NEED TO IMPLEMENT"
+        return
+    elsif innerCorners.size == 3
+        distances = innerCorners.combination(2).map { |pair| [(pair[1][0]-pair[0][0]) / cubeSize, (pair[1][1] - pair[0][1]) / cubeSize]}
+        puts distances.inspect
+    elsif innerCorners.size == 4
+        puts "NEED TO IMPLEMENT"
+        return
+    end
+end
 
 instructions = lines[-1].scan(/\d+[RL]/).map { |pair| [pair[0..-2].to_i, directions[pair[-1]]] }
 instructions.append([lines[-1][-1].to_i, 0])
@@ -53,6 +107,10 @@ cleaned.map(&:chars).transpose.map(&:join).each_with_index do |line,index|
     end
 end
 
+been = Array.new(cleaned.size) { Array.new(lengthPad, -1) }
+
+been[pos[0]][pos[1]]=0
+
 direction = 0
 instructions.each do |distance, turn|
     if direction == 0
@@ -64,6 +122,8 @@ instructions.each do |distance, turn|
                 end
                 if nC == '.'
                     pos[1] += 1
+                    been[pos[0]][pos[1]]=direction
+                    # puts  "0a. Moving to #{pos.inspect}"
                     next
                 end
             end
@@ -71,6 +131,8 @@ instructions.each do |distance, turn|
                 break
             end
             pos[1] = lefts[pos[0]]
+            # puts  "0b. Moving to #{pos.inspect}"
+            been[pos[0]][pos[1]]=direction
         end
     elsif direction == 1
         (1..distance).each do 
@@ -81,6 +143,8 @@ instructions.each do |distance, turn|
                 end
                 if nC == '.'
                     pos[0] += 1
+                    been[pos[0]][pos[1]]=direction
+                    # puts  "1a. Moving to #{pos.inspect}"
                     next
                 end
             end
@@ -88,6 +152,8 @@ instructions.each do |distance, turn|
                 break
             end
             pos[0] = tops[pos[1]]
+            been[pos[0]][pos[1]]=direction
+            # puts  "1b. Moving to #{pos.inspect}"
         end
     elsif direction == 2
         (1..distance).each do 
@@ -98,6 +164,8 @@ instructions.each do |distance, turn|
                 end
                 if nC == '.'
                     pos[1] -= 1
+                    been[pos[0]][pos[1]]=direction
+                    # puts  "2a. Moving to #{pos.inspect}"
                     next
                 end
             end
@@ -105,6 +173,8 @@ instructions.each do |distance, turn|
                 break
             end
             pos[1] = rights[pos[0]]
+            been[pos[0]][pos[1]]=direction
+            # puts  "2b. Moving to #{pos.inspect}"
         end
     elsif direction == 3
         (1..distance).each do 
@@ -115,6 +185,8 @@ instructions.each do |distance, turn|
                 end
                 if nC == '.'
                     pos[0] -= 1
+                    been[pos[0]][pos[1]]=direction
+                    # puts  "3a. Moving to #{pos.inspect}"
                     next
                 end
             end
@@ -122,10 +194,45 @@ instructions.each do |distance, turn|
                 break
             end
             pos[0] = bottoms[pos[1]]
+            been[pos[0]][pos[1]]=direction
+            # puts  "3b. Moving to #{pos.inspect}"
         end
     end
 
     direction = (direction + turn) % 4
+    # puts  "Turning to #{direction}"
+    # puts "Go #{distance}, turn #{turn}, facing #{direction}"
 end 
 
+cleaned.each_with_index do |line, yInd|
+    str = ""
+    line.each_char.with_index do |c, xInd|
+        b = been[yInd][xInd]
+        if [yInd, xInd] == [148, 35]
+            str +='O'
+        else
+            str += c
+        end
+=begin
+        if b == -1
+            str += c
+        elsif b == 0
+            str += ">"
+        elsif b == 1
+            str += "v"
+        elsif b == 2
+            str += "<"
+        elsif b == 3
+            str += "^"
+        end
+=end
+    end
+    # puts str
+end
+
+puts pos.inspect
+puts direction
+
 puts "#{1000 * (pos[0]+1) + 4 * (pos[1]+1) +direction}"
+
+wrapCube(cubeSize, cleaned)
