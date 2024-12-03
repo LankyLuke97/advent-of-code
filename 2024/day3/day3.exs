2b8f774aafea0075303129d759e8de0e18c09918
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-03 18:15:46 +0000

    2024, Day 3 part 1 elixir

--- 2024/day3/day3.exs
+++ 2024/day3/day3.exs
@@ -1,6 +1,13 @@
 defmodule Day3 do
-  def part1(_path) do
-    0
+  def part1(path) do
+    File.stream!(path)
+    |> Enum.map(&String.trim/1)
+    |> Enum.join()
+    |> then(fn input ->
+      Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)/, input)
+      |> Enum.map(fn [_, x, y] -> {String.to_integer(x), String.to_integer(y)} end)
+    end)
+    |> Enum.reduce(0, fn {x, y}, acc -> acc + (x * y) end)
   end
 
   def part2(_path) do
@@ -8,7 +15,7 @@ defmodule Day3 do
   end
 end
 
-test1_correct = 0
+test1_correct = 161
 test2_correct = 0
 test = Day3.part1("day3/data/test.txt")
 if test != test1_correct do
