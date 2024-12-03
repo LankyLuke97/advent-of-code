62d49feaca8923b1af47373846957b48eee5fe8b
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-03 19:31:02 +0000

    2024, Day 3 part 2 elixir

--- 2024/day3/day3.exs
+++ 2024/day3/day3.exs
@@ -10,13 +10,28 @@ defmodule Day3 do
     |> Enum.reduce(0, fn {x, y}, acc -> acc + (x * y) end)
   end
 
-  def part2(_path) do
-    0
+  def part2(path) do
+    File.stream!(path)
+    |> Enum.map(&String.trim/1)
+    |> Enum.join()
+    |> then(fn input ->
+      Regex.scan(~r/mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/, input)|> Enum.map(fn
+        ["mul(" <> _ = _, x, y] -> [String.to_integer(x), String.to_integer(y)]
+        [match] -> [match]
+      end)
+    end)
+    |> Enum.reduce([0, true], fn
+      [x, y], [acc, true] -> [acc + (x * y), true]
+      ["do()"], [acc, false] -> [acc, true]
+      ["don't()"], [acc, true] -> [acc, false]
+      _, [acc, enabled] -> [acc, enabled]
+    end)
+    |> Enum.at(0)
   end
 end
 
 test1_correct = 161
-test2_correct = 0
+test2_correct = 48
 test = Day3.part1("day3/data/test.txt")
 if test != test1_correct do
     raise "Part 1 test failed; it returned #{test} instead of #{test1_correct}"
@@ -24,7 +39,7 @@ end
 {u_secs, ans} = :timer.tc(fn -> Day3.part1("day3/data/input.txt") end)
 IO.puts "Part 1 answer from elixir is: #{ans}, returned in #{u_secs/1000} ms"
 
-test = Day3.part2("day3/data/test.txt")
+test = Day3.part2("day3/data/test2.txt")
 if test != test2_correct do
     raise "Part 2 test failed; it returned #{test} instead of #{test2_correct}"
 end
