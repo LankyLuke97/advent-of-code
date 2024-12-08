d7f013b93b6bfe970bd0236311170bafe6a956ca
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-08 08:04:02 +0000

    2024, Day 8 part 2

--- 2024/day8/day8.py
+++ 2024/day8/day8.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import math
 from pathlib import Path
 from time import perf_counter
 
@@ -19,7 +20,7 @@ def part1(test=False, file_path=None):
     for i, c in enumerate(inp):
         if c == '.': continue
         nodes[c].append((i // n, i % n))
-        
+
     for _, locations in nodes.items():
         for i in range(len(locations)-1):
             for j in range(i+1, len(locations)):
@@ -33,14 +34,45 @@ def part1(test=False, file_path=None):
     return len(antinodes), end_time - start_time
 
 def part2(test=False, file_path=None):
-    # inp = load_input(test, file_path)
+    inp = load_input(test, file_path)
     start_time = perf_counter()
+    inp = [line.strip() for line in inp if line.strip()]
+    n = len(inp)
+    inp = "".join(inp)
+    antinodes = set()
+    nodes = defaultdict(list)
+
+    for i, c in enumerate(inp):
+        if c == '.': continue
+        nodes[c].append((i // n, i % n))
+
+    for _, locations in nodes.items():
+        if len(locations) == 1:
+            continue
+        antinodes = antinodes.union(set(locations))
+        for i in range(len(locations)-1):
+            for j in range(i+1, len(locations)):
+                diff = (locations[i][0] - locations[j][0], locations[i][1] - locations[j][1])
+                denom = math.gcd(diff[0], diff[1])
+                diff = (diff[0] / denom, diff[1] / denom)
+                steps = 0
+                while True:
+                    steps += 1
+                    antinode = (locations[i][0] - (diff[0] * steps), locations[i][1] - (diff[1] * steps))
+                    if not (0 <= antinode[0] < n and 0 <= antinode[1] < n): break
+                    antinodes.add(antinode)
+                steps = 0
+                while True:
+                    steps += 1
+                    antinode = (locations[i][0] + (diff[0] * steps), locations[i][1] + (diff[1] * steps))
+                    if not (0 <= antinode[0] < n and 0 <= antinode[1] < n): break
+                    antinodes.add(antinode)
 
     end_time = perf_counter()
-    return 0, end_time - start_time
+    return len(antinodes), end_time - start_time
 
 test1_correct = 14
-test2_correct = 0
+test2_correct = 34
 test, _ = part1(test=True)
 assert test == test1_correct, f'Part 1 test failed; it returned {test} instead of {test1_correct}'
 part1_ans, part1_time = part1()
