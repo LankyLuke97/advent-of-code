2a5d9562577af0ef4f4c8fd6034b6335a3fd7674
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-08 09:05:40 +0000

    2024, Day 4 part 1 elixir

--- 2024/day4/day4.exs
+++ 2024/day4/day4.exs
@@ -1,6 +1,40 @@
 defmodule Day4 do
-  def part1(_path) do
-    0
+  defp extract_diagonals(matrix) do
+    height = length(matrix)
+    width = length(List.first(matrix))
+
+    0..(height + width - 2)
+    |> Enum.map(fn diag_idx ->
+      for row <- 0..(height - 1),
+          col <- 0..(width - 1),
+          row + col == diag_idx,
+          do: Enum.at(Enum.at(matrix, row, []), col, nil)
+    end)
+  end
+
+  def part1(path) do
+    File.stream!(path)
+    |> Enum.map(&String.trim/1)
+    |> Enum.map(&String.graphemes/1)
+    |> then(fn lines ->
+      forward = Enum.map(lines, &Enum.join/1)
+      down = lines
+            |> List.zip()
+            |> Enum.map(&Enum.join(Tuple.to_list(&1)))
+      left_right_diag = extract_diagonals(lines)
+      |> Enum.map(&Enum.join(&1))
+      right_left_diag = extract_diagonals(Enum.map(lines, &Enum.reverse/1))
+      |> Enum.map(&Enum.join(&1))
+      [forward, down, left_right_diag, right_left_diag]
+    end)
+    |> Enum.map(fn direction ->
+      Enum.map(direction, fn line ->
+        search_pattern = ~r/(?<=X)MA(?=S)|(?<=S)AM(?=X)/
+        Enum.count(Regex.scan(search_pattern, line))
+      end)
+      |> Enum.sum()
+    end)
+    |> Enum.sum()
   end
 
   def part2(_path) do
@@ -8,7 +42,7 @@ defmodule Day4 do
   end
 end
 
-test1_correct = 0
+test1_correct = 18
 test2_correct = 0
 test = Day4.part1("day4/data/test.txt")
 if test != test1_correct do
