13588c46d59cebeb0ab8d3aabef0c3c187b4986a
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-09 12:27:56 +0000

    2024, Day 9 part 2

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -42,14 +42,42 @@ def part1(test=False, file_path=None):
     return checksum, end_time - start_time
 
 def part2(test=False, file_path=None):
-    # inp = load_input(test, file_path)
+    inp = [int(i) for i in list(load_input(test, file_path)[0].strip())]
     start_time = perf_counter()
+    inp = [[i, ind // 2 if ind % 2 == 0 else -1] for ind, i in enumerate(inp)]
+    file_pointer = len(inp) - 1
+
+    while file_pointer > 0:
+        num_blocks, file_id = inp[file_pointer]
+        empty_pointer = 1
+        while empty_pointer < file_pointer and inp[empty_pointer][0] < num_blocks:
+            empty_pointer += 2
+        if empty_pointer < file_pointer:
+            inp[empty_pointer][0] -= num_blocks
+            inp.pop(file_pointer)
+            inp.insert(file_pointer, [num_blocks, -1])
+            inp.insert(empty_pointer, [num_blocks, file_id])
+            inp.insert(empty_pointer, [0, -1])
+        else:
+            file_pointer -= 2
+
+    current_unit = 0
+    checksum = 0
+
+    while inp:
+        num_blocks, file_id = inp.pop(0)
+        if file_id == -1 or not num_blocks:
+            current_unit += num_blocks
+            continue
+        for _ in range(num_blocks):
+            checksum += (file_id * current_unit)
+            current_unit += 1
 
     end_time = perf_counter()
-    return 0, end_time - start_time
+    return checksum, end_time - start_time
 
 test1_correct = 1928
-test2_correct = 0
+test2_correct = 2858
 test, _ = part1(test=True)
 assert test == test1_correct, f'Part 1 test failed; it returned {test} instead of {test1_correct}'
 part1_ans, part1_time = part1()
