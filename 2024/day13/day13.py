c7b61586284ccc8a4d532aedf0b504ba3dea3f9a
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-13 13:39:52 +0000

    2024, Day 13 part 2

--- 2024/day13/day13.py
+++ 2024/day13/day13.py
@@ -7,13 +7,13 @@ def load_input(test=False, file_path=None):
     with open(file_path, 'r') as f:
         return [line.strip() for line in f.readlines() if line.strip()]
 
-def part1(test=False, file_path=None):
-    def calculate_tokens(a, b, loc):
-        b_presses = (loc[0] * a[1] - loc[1] * a[0]) / (b[0] * a[1] - b[1] * a[0])
-        if not ((int(b_presses)) == b_presses): return 0
-        a_presses = (loc[0] - (b[0] * b_presses)) / (a[0])
-        return int(a_presses*3 + b_presses)
+def calculate_tokens(a, b, loc):
+    b_presses = (loc[0] * a[1] - loc[1] * a[0]) / (b[0] * a[1] - b[1] * a[0])
+    if not ((int(b_presses)) == b_presses): return 0
+    a_presses = (loc[0] - (b[0] * b_presses)) / (a[0])
+    return int(a_presses*3 + b_presses)
 
+def part1(test=False, file_path=None):
     inp = load_input(test, file_path)
     start_time = perf_counter()
     tokens = 0
@@ -28,14 +28,21 @@ def part1(test=False, file_path=None):
     return tokens, end_time - start_time
 
 def part2(test=False, file_path=None):
-    # inp = load_input(test, file_path)
+    inp = load_input(test, file_path)
     start_time = perf_counter()
+    tokens = 0
+    
+    while inp:
+        button_a = tuple([int(m) for m in re.findall(r'(?<=[X|Y]\+)\d+', inp.pop(0))])
+        button_b = tuple([int(m) for m in re.findall(r'(?<=[X|Y]\+)\d+', inp.pop(0))])
+        location = tuple([10000000000000 + int(m) for m in re.findall(r'(?<=[X|Y]=)\d+', inp.pop(0))])
+        tokens += calculate_tokens(button_a, button_b, location)
 
     end_time = perf_counter()
-    return 0, end_time - start_time
+    return tokens, end_time - start_time
 
 test1_correct = 480
-test2_correct = 0
+test2_correct = 875318608908
 test, _ = part1(test=True)
 assert test == test1_correct, f'Part 1 test failed; it returned {test} instead of {test1_correct}'
 part1_ans, part1_time = part1()
