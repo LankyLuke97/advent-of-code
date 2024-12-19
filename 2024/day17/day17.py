4125704bb652e86d2d70a92eaabbe7dbc9014333
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-19 11:06:17 +0000

    2024, Day 17 part 2

--- 2024/day17/day17.py
+++ 2024/day17/day17.py
@@ -1,3 +1,4 @@
+import math
 from pathlib import Path
 from time import perf_counter
 
@@ -34,47 +35,41 @@ def part1(test=False, file_path=None):
     return ','.join([str(i) for i in out]), end_time - start_time
 
 def part2(test=False, file_path=None):
-    inp = load_input(test, file_path)
-    start_time = perf_counter()
-
-    orig_registers = [int(i.split(':')[1].strip()) for i in inp[:3]]
-    program = [int(i) for i in inp[3].split(':')[1].strip().split(',')]
-    pos = 1
-    a_reg = 1
-    octals = []
-
-    while True:
-        registers = [a_reg, orig_registers[1], orig_registers[2]]
+    def run_program(program, a):
+        b, c = 0, 0
         pointer = 0
         out = []
 
         while pointer < len(program) - 1:
-            instruction = program[pointer]
-            literal_operand = program[pointer + 1]
-            combo_operand = literal_operand if literal_operand < 4 else registers[literal_operand % 4]
+            instruction, literal_operand = program[pointer:pointer+2]
+            op = {0:0, 1:1, 2:2, 3:3, 4:a, 5:b, 6:c}
 
-            if instruction == 0: registers[0] = int(registers[0] * (2 ** -combo_operand))
-            elif instruction == 1: registers[1] = registers[1] ^ literal_operand
-            elif instruction == 2: registers[1] = combo_operand % 8
-            elif instruction == 3 and registers[0]: pointer = literal_operand - 2
-            elif instruction == 4: registers[1] = registers[1] ^ registers[2]
-            elif instruction == 5: out.append(combo_operand % 8)
-            elif instruction == 6: registers[1] = int(registers[0] * (2 ** -combo_operand))
-            elif instruction == 7: registers[2] = int(registers[0] * (2 ** -combo_operand))
+            if instruction == 0: a = int(a * (2 ** -op[literal_operand]))
+            elif instruction == 1: b = b ^ literal_operand
+            elif instruction == 2: b = op[literal_operand] % 8
+            elif instruction == 3 and a: pointer = literal_operand - 2
+            elif instruction == 4: b = b ^ c
+            elif instruction == 5: out.append(op[literal_operand] % 8)
+            elif instruction == 6: b = int(a * (2 ** -op[literal_operand]))
+            elif instruction == 7: c = int(a * (2 ** -op[literal_operand]))
             pointer += 2
-        if out == program: return a_reg, perf_counter() - start_time
-        if out[0] == program[-pos]:
-            print(f'Matches {out[0]} to program[-{pos}] with a_reg = {a_reg}')
-            octals.append(a_reg)
-            if pos == len(program): break
-            a_reg = 8 ** pos
-            print(f'Setting {a_reg}')
-            pos += 1
-            continue
-        else: a_reg += 1
-    print(octals)
+        return out
+
+    inp = load_input(test, file_path)
+    start_time = perf_counter()
+    program = [int(i) for i in inp[3].split(':')[1].strip().split(',')]
+    todo = [(0,1)]
+    min_ans = math.inf
+        
+    while todo:
+        a, i = todo.pop(0)
+        for a in range(a, a+8):
+            if run_program(program, a) == program: min_ans = min(min_ans, a)
+            elif i <= len(program) and run_program(program, a) == program[-i:]:
+                todo.append((a*8, i+1))
+
     end_time = perf_counter()
-    return sum(octals), end_time - start_time
+    return min_ans, end_time - start_time
 
 test1_correct = '4,6,3,5,6,3,5,2,1,0'
 test2_correct = 117440
