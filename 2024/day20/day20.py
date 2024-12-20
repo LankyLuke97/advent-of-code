231ecb9dd070c557f540a84908683463135ce824
LankyLuke97 <lukehowden@rocketmail.com>
2024-12-20 16:17:48 +0000

    2024, Day 20 part 2
    Includes reefactor for part 1. Didn't notice the rubric stating that
    there was only one path through the maze, which massively reduces the
    search space for part 2.

--- 2024/day20/day20.py
+++ 2024/day20/day20.py
@@ -1,4 +1,3 @@
-from collections import Counter
 from copy import deepcopy
 import heapq
 import math
@@ -11,65 +10,66 @@ def load_input(test=False, file_path=None):
         return [line.strip() for line in f.readlines() if line.strip()]
 
 class Node:
-    def __init__(self, y, x, f, g):
+    def __init__(self, y, x, parent, f, g):
         self.y = y
         self.x = x
+        self.parent = parent
         self.f = f
         self.g = g
     def __lt__(self, other):
         return (self.f < other.f)
 
-def a_star(grid, start, end, min_steps=math.inf, closed_nodes=None, shortcut=None):
-        directions = [(-1,0),(0,1),(1,0),(0,-1)]
-        open_nodes = [start]
-        closed_nodes = [[math.inf] * len(grid[0]) for _ in range(len(grid))] if not closed_nodes else closed_nodes
-        
-        while open_nodes:
-            cur_node = heapq.heappop(open_nodes)
+def a_star(grid, start, end):
+    directions = [(-1,0),(0,1),(1,0),(0,-1)]
+    open_nodes = [start]
+    closed_nodes = [[math.inf] * len(grid[0]) for _ in range(len(grid))]
+    min_steps = math.inf
+    end_node = None
+    
+    while open_nodes:
+        cur_node = heapq.heappop(open_nodes)
 
-            if cur_node.y == end[0] and cur_node.x == end[1]: min_steps = min(min_steps, cur_node.g); continue
-            if cur_node.g >= closed_nodes[cur_node.y][cur_node.x]: continue
-            closed_nodes[cur_node.y][cur_node.x] = cur_node.g
-            for direction in directions:
-                next_position = (cur_node.y + direction[0], cur_node.x + direction[1])
-                new_g = cur_node.g + 1
-                if new_g >= closed_nodes[next_position[0]][next_position[1]] or grid[next_position[0]][next_position[1]]: continue
-                h = abs(end[0] - next_position[0]) + abs(end[1] - next_position[1])
-                f = new_g + h
-                new_node = Node(next_position[0], next_position[1], f, new_g)
-                heapq.heappush(open_nodes, new_node)
-            if shortcut and (cur_node.y, cur_node.x) == shortcut[0]:
-                next_position = shortcut[1]
-                new_g = cur_node.g + shortcut[2]
-                if new_g >= closed_nodes[next_position[0]][next_position[1]] or grid[next_position[0]][next_position[1]]: continue
-                h = abs(end[0] - next_position[0]) + abs(end[1] - next_position[1])
-                f = new_g + h
-                new_node = Node(next_position[0], next_position[1], f, new_g)
-                heapq.heappush(open_nodes, new_node)
-        return min_steps, closed_nodes
+        if cur_node.y == end[0] and cur_node.x == end[1]:
+            end_node = cur_node
+            break
+        
+        if cur_node.g >= closed_nodes[cur_node.y][cur_node.x]: continue
+        closed_nodes[cur_node.y][cur_node.x] = cur_node.g
+        for direction in directions:
+            next_position = (cur_node.y + direction[0], cur_node.x + direction[1])
+            new_g = cur_node.g + 1
+            if new_g >= closed_nodes[next_position[0]][next_position[1]] or grid[next_position[0]][next_position[1]]: continue
+            h = abs(end[0] - next_position[0]) + abs(end[1] - next_position[1])
+            f = new_g + h
+            new_node = Node(next_position[0], next_position[1], cur_node, f, new_g)
+            heapq.heappush(open_nodes, new_node)
+
+    path = []
+    cur_node = end_node
+
+    while cur_node:
+        path.append((cur_node.y, cur_node.x))
+        cur_node = cur_node.parent
+    return min_steps, path[::-1]
 
 def part1(test=False, file_path=None):
     inp = load_input(test, file_path)
     start_time = perf_counter()
 
+    dist = 10 if test else 100
+
     grid = [[0] * len(inp[0]) for _ in range(len(inp))]
     start = end = None
     for y, line in enumerate(inp):
         for x, c in enumerate(line):
-            if c == 'S': start = Node(y, x, 0, 0)
+            if c == 'S': start = Node(y, x, None, 0, 0)
             elif c == 'E': end = (y, x)
             elif c == '#': grid[y][x] = -1
-    min_path, _ = a_star(grid, start, end)
-    cheats = []
-    for y in range(1, len(grid)-1):
-        for x in range(1, len(grid[0]) -1):
-            if not grid[y][x]: continue
-            if (grid[y-1][x] or grid[y+1][x]) and (grid[y][x-1] or grid[y][x+1]): continue
-            grid[y][x] = 0
-            cheats.append(min_path - a_star(grid, start, end, min_path)[0])
-            grid[y][x] = -1
-    
-    cheats = sum([value for key, value in Counter([cheat for cheat in cheats if cheat]).items() if key >= 100])
+    _, path = a_star(grid, start, end)
+    cheats = 0
+    for i, (y, x) in enumerate(path[:-dist]):
+        potential = {(y-2, x), (y, x+2), (y+2, x), (y, x-2)}
+        cheats += sum([1 for (c_y, c_x) in path[i+dist+2:] if (c_y, c_x) in potential])
 
     end_time = perf_counter()
     return cheats, end_time - start_time
@@ -79,53 +79,40 @@ def part2(test=False, file_path=None):
     start_time = perf_counter()
 
     grid = [[0] * len(inp[0]) for _ in range(len(inp))]
+    directions = [(-1,0),(0,1),(1,0),(0,-1)]
     start = end = None
     for y, line in enumerate(inp):
         for x, c in enumerate(line):
-            if c == 'S': start = Node(y, x, 0, 0)
+            if c == 'S': start = Node(y, x, None, 0, 0)
             elif c == 'E': end = (y, x)
             elif c == '#': grid[y][x] = -1
-    min_path, closed_nodes = a_star(grid, start, end)
+    _, path = a_star(grid, start, end)
+    path_set = set(path)
 
-    directions = [(-1,0),(0,1),(1,0),(0,-1)]
-    cheats = []
-    for y in range(1, len(grid)-1):
-        for x in range(1, len(grid[0]) -1):
-            if grid[y][x]: continue
-            checked_cheats = set()
-            for length in range(20, 1, -1):
-                for y_offset in range(-length, length + 1):
-                    x_offset = length - abs(y_offset)
-                    for _dir in (-1, 1):
-                        cheat_start = (y+y_offset, x+(_dir*x_offset))
-                        if (cheat_start) in checked_cheats: continue
-                        checked_cheats.add(cheat_start)
-                        if cheat_start == end:
-                            cheats.append(min_path - (closed_nodes[y][x] + length))
-                            continue
-                        if not (0 < cheat_start[0] < len(grid) - 1 and 0 < cheat_start[1] < len(grid[0]) - 1): continue
-                        if grid[cheat_start[0]][cheat_start[1]]: continue
-                        _closed_nodes = deepcopy(closed_nodes)
-                        _closed_nodes[cheat_start[0]][cheat_start[1]] = closed_nodes[y][x] + length
-                        if _closed_nodes[cheat_start[0]][cheat_start[1]] >= closed_nodes[cheat_start[0]][cheat_start[1]]: continue
-                        # cheats.append(min_path - a_star(grid, Node(cheat_start[0], cheat_start[1], 0, _closed_nodes[cheat_start[0]][cheat_start[1]]), end, min_path, _closed_nodes)[0])
-                        cheats.append(min_path - a_star(grid, start, end, min_path, shortcut=((y,x),cheat_start,length))[0])
-                        # if(cheats[-1]): print(f'Cheat starting at {(y,x)} and ending at {cheat_start} saves {cheats[-1]} picoseconds.')
+    dist = 70 if test else 100
 
-    cheats = Counter([cheat for cheat in cheats if cheat])
-    cheats = sorted([(value, key) for key, value in cheats.items()], key=lambda x: x[1])
-    # for key, value in cheats: print(f'There are {key} cheats that save {value} picoseconds.')
-    # cheats = sum([value for key, value in Counter([cheat for cheat in cheats if cheat]).items() if key >= 100])
-
+    cheats = 0
+    cheat_amounts = []
+    for i, (y, x) in enumerate(path[:-dist]):
+        potential = {}
+        for length in range(20, 1, -1):
+            for y_offset in range(-length, length + 1):
+                x_offset = length - abs(y_offset)
+                for _dir in (-1, 1):
+                    if (y+y_offset, x+(_dir*x_offset)) not in path_set: continue
+                    potential[(y+y_offset, x+(_dir*x_offset))] = length
+        for j, (c_y, c_x) in enumerate(path[i+dist:]):
+            if (c_y, c_x) not in potential: continue
+            if j - potential[(c_y, c_x)] >= 0: cheats += 1
     end_time = perf_counter()
-    return sum([key for key, value in cheats if value >= 100]), end_time - start_time
+    return cheats, end_time - start_time
 
-test1_correct = 0
-test2_correct = 0
+test1_correct = 10
+test2_correct = 41
 test, _ = part1(test=True)
 assert test == test1_correct, f'Part 1 test failed; it returned {test} instead of {test1_correct}'
-#part1_ans, part1_time = part1()
-#print(f'Part 1 answer is: {part1_ans}, returned in {part1_time * 1000:.3f} ms')
+part1_ans, part1_time = part1()
+print(f'Part 1 answer is: {part1_ans}, returned in {part1_time * 1000:.3f} ms')
 test, _ = part2(test=True)
 assert test == test2_correct, f'Part 2 test failed; it returned {test} instead of {test2_correct}'
 part2_ans, part2_time = part2()
