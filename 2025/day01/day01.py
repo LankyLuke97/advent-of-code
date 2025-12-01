a9eaccf2df6368f3aa235738a376a7de0b5f5011
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-01 15:09:13 +0000

    2025, day 1 part 2

--- 2025/day01/day01.py
+++ 2025/day01/day01.py
@@ -8,3 +8,17 @@ with open('data.txt', mode='r', encoding='utf8') as data:
         if not cur: password += 1
 
 print(password)
+
+password = 0
+cur = 50
+
+with open('data.txt', mode='r', encoding='utf8') as data:
+    for line in data.readlines():
+        dir, (p, dist) = 1 if line[0] == 'R' else -1, divmod(int(line[1:]), 100)
+        password += p
+        if cur != 0:
+            if dir < 0 and cur - dist <= 0: password += 1
+            elif dir > 0 and cur + dist >= 100: password += 1
+        cur = (cur + dir * dist) % 100
+
+print(password)
