5c4a6161bfc93c5b3df12a51718a177f17fa904f
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-02 11:03:13 +0000

    2025, day 2 part 2

--- 2025/day02/day02.py
+++ 2025/day02/day02.py
@@ -1,18 +1,23 @@
 from math import ceil
 from pathlib import Path
+import re
 
 def part_one(file):
     ans = 0
     with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
         for r in data.readlines()[0].split(','):
-            start, end = r.split('-')
-            cur = start[:len(start)//2]
-            if not cur: cur = '1'
-            start, end = int(start), int(end)
-            while int(cur+cur) < start: cur = str(int(cur)+1)
-            while int(cur+cur) <= end:
-                ans += int(cur+cur)
-                cur = str(int(cur)+1)
+            start, end = map(int, r.split('-'))
+            for i in range(start, end+1):
+                if re.fullmatch(r'(\d+)\1', str(i)): ans += i
+    return ans
+
+def part_two(file):
+    ans = 0
+    with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
+        for r in data.readlines()[0].split(','):
+            start, end = map(int, r.split('-'))
+            for i in range(start, end+1):
+                if re.fullmatch(r'(\d+)\1+', str(i)): ans += i
     return ans
 
 assert 1227775554 == part_one('test.txt')
