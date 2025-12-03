29098032c3a0daa65ef1c0ef35e0f08d36171cc4
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-03 19:13:14 +0000

    2025, day 3 part 2 misunderstanding something
    Incomplete dp attempt. I think there was something to this, but I
    couldn't figure out where I was going wrong. Worth exploring when bored.

--- 2025/day03/day03.py
+++ 2025/day03/day03.py
@@ -8,7 +8,7 @@ def part_one(file):
         for line in data.readlines():
             line = line.strip()
             l = len(line)
-            dp = [[('0','0')] * l for _ in range(2)]
+            dp = [[('0',0)] * l for _ in range(2)]
             dp[0][-2] = (line[-2], l-2)
             dp[1][-1] = (line[-1], l-1)
             for idx in range(l-2,-1,-1):
@@ -26,10 +26,39 @@ def part_one(file):
 
 def part_two(file):
     ans = 0
+    with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
+        for line in data.readlines():
+            line = line.strip()
+            l = len(line)
+            dp = [[('0',0)] * l for _ in range(12)]
+            for i in range(len(dp)):
+                for j in range(i-12, 0):
+                    dp[i][i-j] = (line[i-12], l+i-12)
+
+            for row in dp: print(row)
+            input()
+            for idx in range(l-2,-1,-1):
+                for row in dp:
+                    if row[idx+1][0] <= line[idx] and row[idx][1] <= idx: row[idx] = (line[idx], idx)
+                    else: row[idx] = row[idx+1]
+                print(f'-----{idx:02}-----')
+                for row in dp: print(row)
+                input()
+
+            string = ""
+            idx = 0
+            for row in dp:
+                c, idx = row[idx]
+                string += c
+                idx += 1
+            print(string)
+            for row in dp: print(row)
+            input()
+            ans += int(string)
     return ans
 
 assert 357 == part_one('test.txt')
 print(part_one('data.txt'))
 
-assert 0 == part_two('test.txt')
+assert 3121910778619 == part_two('test.txt')
 print(part_two('data.txt'))
