10df9595d44a11c3b97305fc7e823e82f6f1ec7a
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-04 08:42:44 +0000

    2025, day 4 part 2

--- 2025/day04/day04.py
+++ 2025/day04/day04.py
@@ -17,10 +17,31 @@ def part_one(file):
 
 def part_two(file):
     ans = 0
+    with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
+        check = []
+        rolls = set()
+        directions = ((-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1))
+        mx, my = 0, 0
+        for y, line in enumerate(data.readlines()):
+            my = y
+            mx = len(line)-1
+            for x, c in enumerate(line.strip()):
+                if '@' == c:
+                    check.append((y,x))
+                    rolls.add((y,x))
+        while check:
+            y, x = check.pop()
+            if (y, x) not in rolls: continue
+            count = 0
+            neighbours = [(y+dy, x+dx) for dy, dx in directions if (y+dy, x+dx) in rolls]
+            if len(neighbours) < 4:
+                ans += 1
+                check.extend(neighbours)
+                rolls.remove((y, x))
     return ans
 
 assert 13 == part_one('test.txt')
 print(part_one('data.txt'))
 
-assert 0 == part_two('test.txt')
+assert 43 == part_two('test.txt')
 print(part_two('data.txt'))
