a0dbd3759bb0c7908b89b40d796720d515028fce
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-04 08:14:55 +0000

    2025, day 4 part 1

---
+++ 2025/day04/day04.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+def part_one(file):
+    ans = 0
+    with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
+        rolls = set()
+        directions = ((-1,0),(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1))
+        for y, line in enumerate(data.readlines()):
+            for x, c in enumerate(line.strip()):
+                if '@' == c: rolls.add((y,x))
+        for y, x in rolls:
+            count = 0
+            for dy, dx in directions: count += 1 if (y+dy, x+dx) in rolls else 0
+            if count < 4: ans += 1
+        
+    return ans
+
+def part_two(file):
+    ans = 0
+    return ans
+
+assert 13 == part_one('test.txt')
+print(part_one('data.txt'))
+
+assert 0 == part_two('test.txt')
+print(part_two('data.txt'))
