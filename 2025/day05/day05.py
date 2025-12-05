b81021c0e9898643ec281641458f937e73bdf47a
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-05 16:55:37 +0000

    2025, day 5 part 2

--- 2025/day05/day05.py
+++ 2025/day05/day05.py
@@ -26,13 +26,26 @@ def part_one(file):
     return ans
 
 def part_two(file):
-    ans = 0
+    ranges = []
     with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
-        pass
-    return ans
+        for line in data:
+            if not line.strip(): break
+            start, end = list(map(int, line.split('-')))
+            idx = bisect.bisect_right(ranges, start, key=lambda x: x[0])
+            if idx > 0 and start <= ranges[idx-1][1] + 1:
+                idx -= 1
+                start, end = ranges[idx][0], max(end, ranges[idx][1])
+                ranges[idx] = [start, end]
+            else: ranges = ranges[:idx] + [[start, end]] + ranges[idx:]
+            merge_idx = idx
+            while merge_idx < len(ranges) - 1 and end >= ranges[merge_idx+1][0] - 1:
+                merge_idx += 1
+                end = max(end, ranges[merge_idx][1])
+            if merge_idx != idx: ranges = ranges[:idx] + [[start, end]] + ranges[merge_idx+1:]
+    return sum([b-a+1 for a, b in ranges])
 
 assert 3 == part_one('test.txt')
 print(part_one('data.txt'))
 
-assert 0 == part_two('test.txt')
+assert 14 == part_two('test.txt')
 print(part_two('data.txt'))
