aef4439a505a1a214520ebbb8b5f1ec42f4c1d77
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-07 10:47:15 +0000

    2025, day 7 part 2

--- 2025/day07/day07.py
+++ 2025/day07/day07.py
@@ -21,11 +21,27 @@ def part_one(file):
 def part_two(file):
     ans = 0
     with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
-        pass
+        for line in data: 
+            start = line.index('S')
+            break
+        splits = set()
+        max_y = 0
+        for y, line in enumerate(data.readlines()):
+            splits |= {(y, x) for x, c in enumerate(line) if '^' == c}
+            max_y = y
+    
+    @cache
+    def rec(y, x):
+        if y >= max_y: return 1
+        if (y, x) not in splits: return rec(y+1, x)
+        return rec(y+1, x-1) + rec(y+1,x+1)
+
+    ans = rec(0, start)
+
     return ans
 
 assert 21 == part_one('test.txt')
 print(part_one('data.txt'))
 
-assert 0 == part_two('test.txt')
+assert 40 == part_two('test.txt')
 print(part_two('data.txt'))
