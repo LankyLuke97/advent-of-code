4833d5b747371980f3b5c50ee3ef08e3dc4f4d32
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-08 11:11:09 +0000

    2025, day 8 part 2

--- 2025/day08/day08.py
+++ 2025/day08/day08.py
@@ -34,11 +34,37 @@ def part_one(file, connections=1000):
 def part_two(file):
     ans = 0
     with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
-        pass
+        positions = [[int(x) for x in l.strip().split(',')] for l in data.readlines()]
+    roots = [i for i in range(len(positions))]
+    sizes = [1 for _ in range(len(positions))]
+    
+    def find_parent(x):
+        if roots[x] != x:
+            roots[x] = find_parent(roots[x])
+            return roots[x]
+
+        return x
+
+    def square_dist(x, y):
+        if len(x) != len(y): raise ValueError(f"Both {x} and {y} must be iterables of numeric data")
+        return sum([(a-b)*(a-b) for a, b in zip(x, y)])
+
+    connections = len(positions)-1
+    for _, p1, p2 in sorted([(square_dist(positions[i], positions[j]), i, j) for i, j in combinations(range(len(positions)), 2)]):
+        x_root, y_root = find_parent(p1), find_parent(p2)
+        if x_root != y_root:
+            connections -= 1
+            if not connections:
+                ans = positions[p1][0] * positions[p2][0]
+                break
+            if sizes[x_root] < sizes[y_root]: x_root, y_root = y_root, x_root
+            roots[y_root] = roots[x_root]
+            sizes[y_root] += sizes[x_root]
+
     return ans
 
 assert 40 == part_one('test.txt', connections=10)
 print(part_one('data.txt'))
 
-assert 0 == part_two('test.txt')
+assert 25272 == part_two('test.txt')
 print(part_two('data.txt'))
