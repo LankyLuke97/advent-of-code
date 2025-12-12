51ced178babf1674ea21edca1af8cea33dfb2f4a
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-12 16:29:33 +0000

    2025, day 10 part 2

--- 2025/day10/day10.py
+++ 2025/day10/day10.py
@@ -1,4 +1,5 @@
 from functools import cache
+from itertools import combinations
 from pathlib import Path
 import re
 
@@ -40,11 +41,31 @@ def part_one(file):
 def part_two(file):
     ans = 0
     with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
-        pass
+        for line in data:
+            _, *buttons, target = line.split()
+            target = tuple(int(x) for x in target[1:-1].split(','))
+            buttons = [[int(x) for x in b[1:-1].split(',')] for b in buttons]
+            buttons = [tuple(int(i in b) for i in range(len(target))) for b in buttons]
+            costs = {}
+            for pattern_len in range(len(buttons)+1):
+                for bs in combinations(range(len(buttons)), pattern_len):
+                    pattern = tuple(map(sum, zip((0,) * len(buttons[0]), *(buttons[i] for i in bs))))
+                    if pattern not in costs: costs[pattern] = pattern_len
+
+            @cache
+            def recurse(goal):
+                if all(i == 0 for i in goal): return 0
+                answer = 1_000_000
+                for pattern, pattern_cost in costs.items():
+                    if all(i <= j and i%2 == j%2 for i, j in zip(pattern, goal)):
+                        new_goal = tuple((j-i)//2 for i, j in zip(pattern, goal))
+                        answer = min(answer, pattern_cost + 2 * recurse(new_goal))
+                return answer
+            ans += recurse(target)
     return ans
 
 assert 7 == part_one('test.txt')
 print(part_one('data.txt'))
 
-assert 0 == part_two('test.txt')
+assert 33 == part_two('test.txt')
 print(part_two('data.txt'))
