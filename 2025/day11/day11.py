05fdda38c150db0caedd47295d7478ac22ee6954
LankyLuke97 <lukehowden@rocketmail.com>
2025-12-12 09:41:12 +0000

    2025, day 11 part 2

--- 2025/day11/day11.py
+++ 2025/day11/day11.py
@@ -17,13 +17,24 @@ def part_one(file):
     return search('you') 
 
 def part_two(file):
-    ans = 0
+    neighbours = {}
     with open(Path(__file__).resolve().parent / file, mode='r', encoding='utf8') as data:
-        pass
-    return ans
+        for line in data:
+            key, value = line.split(':')
+            neighbours[key] = value.strip().split()
+
+    @cache
+    def search(key, dac, fft):
+        if key == 'out': return 1 if dac and fft else 0
+        if key not in neighbours: return 0
+        dac |= 'dac' == key
+        fft |= 'fft' == key
+        return sum([search(neighbour,dac,fft) for neighbour in neighbours[key]])
+
+    return search('svr', False, False) 
 
 assert 5 == part_one('test.txt')
 print(part_one('data.txt'))
 
-assert 0 == part_two('test.txt')
+assert 2 == part_two('test2.txt')
 print(part_two('data.txt'))
